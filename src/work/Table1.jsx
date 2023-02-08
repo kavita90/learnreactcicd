ab5ce52a1475ef189c31cbfaf1f90a78646a9163
Kavita Goel <EE212588@sasken.com>
2023-02-09 00:45:53 +0530

    color change div

--- src/work/Table1.jsx
+++ src/work/Table1.jsx
@@ -3,32 +3,25 @@ import axios from 'axios';
 
 export const Table1 = ()=>{
     const [data, setData] = useState([])
-    let ma = [1,2,3]
-
-  const     fetchapi = async ()=>{
+    const   fetchapi = async ()=>{
       let response = await axios.get('https://jsonplaceholder.typicode.com/users')    
       setData(response.data)
       }
       useEffect(  ()=>{
       fetchapi()    
       },[])    
-      console.log(data) 
-
-      const [cl, setCl] = useState("red")
-      // const [cl, setCl] = useState("red")
-
-      const handleColor = ()=>{
-        console.log("kkkk");
-        setCl("yellow")
-      }
-    return <div className=''>
+      const [cl, setCl] = useState("")
+      const handleColor = (id)=>{
+      if(id === cl)  setCl("")
+      else  setCl(id)
+       }
+    return  <div> <div className=''>
         <table>
             <tr>
                 <th>id</th>
                 <th>email</th>
                 <th>name</th>
                 <th>phone</th>
-
             </tr>
            {
             data.map((item)=>{
@@ -37,16 +30,17 @@ export const Table1 = ()=>{
                     <td>{item.email}</td>
                     <td>{item.name}</td>
                     <td>{item.phone}</td>
-
                 </tr>
             })
            }
         </table>
-
-        
-
- <div style={{backgroundColor: `${cl}`}} onClick={handleColor}>hjhj</div>
-     
-        <div>ytytyy</div>
+        </div>
+        <div>
+        {
+            data.map((item, index)=>{
+              return <div style={{backgroundColor:  cl === index ? "yellow" : "red"}} onClick={()=>{handleColor(index)}}>{item.name}</div>
+            })
+           }
+    </div>
     </div>
 }
